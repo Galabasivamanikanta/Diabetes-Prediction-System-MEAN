0106d39e930c62354cb66286d53b69d28e5014fa
Siva Manikanta <sivamanikanta1013@gmail.com>
2026-04-06 18:58:06 +0530

    Fix Google Login: Update to tj0c-id

--- frontend/src/app/auth.component.ts
+++ frontend/src/app/auth.component.ts
@@ -198,7 +198,7 @@ export class AuthComponent implements OnInit {
       if (typeof google !== 'undefined') {
         clearInterval(interval);
         google.accounts.id.initialize({
-          client_id: '857013288806-sqctidov8716rpqnoocl3t1g8gerpge7.apps.googleusercontent.com',
+          client_id: '857013288806-tj0c7gmg56aooq52rvpacum13nps7pmm.apps.googleusercontent.com',
           callback: (res: any) => this.handleGoogleLogin(res.credential)
         });
         
